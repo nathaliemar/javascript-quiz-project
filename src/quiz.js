66ae2a29bf496daec2bbff546bca9e6553051bec
nathaliemar <n.marten.95@gmail.com>
2025-04-14 15:12:00 +0200

    Day 1 progress

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,34 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
 
-    // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [
+        this.questions[i],
+        (this.questions[j] = [this.questions[j], this.questions[i]]),
+      ];
+    }
+  }
 
-    // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    this.correctAnswers++;
+  }
 
-    // 6. hasEnded()
+  hasEnded() {
+    return this.currentQuestionIndex === this.questions.length;
+  }
 }
