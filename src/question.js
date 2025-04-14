42508bb787f6fb44e03da2fb34fb51665cc390f2
nathaliemar <n.marten.95@gmail.com>
2025-04-14 15:23:18 +0200

    day 1 minor fixes

--- src/question.js
+++ src/question.js
@@ -8,7 +8,7 @@ class Question {
   shuffleChoices() {
     for (let i = this.choices.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [this.choices[i], (this.choices[j] = [this.choices[j], this.choices[i]])];
+      [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
     }
   }
 }
